bb75e1a9f1957890d41eeaa0c0b420ffc34ddcac
Christo Goosen <christo.goosen@vatglobal.com>
2022-04-07 11:20:15 +0200

    Update version number

--- maintenancemode/models.py
+++ maintenancemode/models.py
@@ -4,6 +4,7 @@ from django.db import models
 try:
     from django.utils.translation import ugettext_lazy as _
 except ImportError:
+    # Needed for django 4 which renames ugettext_lazy
     from django.utils.translation import gettext_lazy as _
 from django.db import IntegrityError
 from django.contrib.sites.models import Site
